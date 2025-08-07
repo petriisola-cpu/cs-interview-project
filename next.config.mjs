2df4119d28fed7f1c3ef5235b23efe3c4633efd8
Tim Benniks <tbenniks@gmail.com>
2025-08-07 14:50:37 +0200

    chore: streamline image hostname configuration in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,12 @@ const nextConfig = {
   images: {
     // Configure allowed hostnames for Next.js Image Optimization
     remotePatterns: [
-      { hostname: "images.contentstack.io" },
-      { hostname: "*-images.contentstack.com" },
-      // Optionally add additional hostname from environment variable
-      // This allows for internal testing at Contentstack or additional CDN configurations
-      // Feel free to remove this if not needed
-      ...(process.env.NEXT_PUBLIC_ADDITIONAL_IMAGE_HOSTNAME
-        ? [{ hostname: process.env.NEXT_PUBLIC_ADDITIONAL_IMAGE_HOSTNAME }]
-        : []),
+      ...(process.env.NEXT_PUBLIC_CONTENTSTACK_IMAGE_HOSTNAME
+        ? [{ hostname: process.env.NEXT_PUBLIC_CONTENTSTACK_IMAGE_HOSTNAME }]
+        : [
+            { hostname: "images.contentstack.io" },
+            { hostname: "*-images.contentstack.com" },
+          ]),
     ],
   },
 };
